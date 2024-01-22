15a3d7fedd8c21eab30c3a05cba742d247fb4008
holyseo <128103221+holyseo@users.noreply.github.com>
2024-01-22 11:21:00 +0000

    add hover on icons and links with images on contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,43 +8,35 @@ export function Contact() {
         <div className=" col-span-1 "></div>
         <div className=" col-span-3 text-white">
           <span className=" text-3xl font-bold">Contact</span>
-          <div className="text-md py-8">
-            Seasoned Full Stack Software Engineer with over 8 years of hands-on
-            experience in designing and implementing robust, scalable, and
-            innovative web solutions. Adept at leveraging a comprehensive skill
-            set encompassing front-end and back-end technologies
+          <div className="text-md py-8 max-w-4xl">
+            I am highly skilled and committed to becoming a successful Software
+            Developer. I have gained valuable experience and knowledge in
+            industry best practices as a junior full-stack developer and web
+            developer.
           </div>
-          <div className="flex flex-row justify-start gap-3">
-            <div className="my-auto">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="17"
-                height="17"
-                viewBox="0 0 17 17"
-                fill="none"
-              >
-                <path
-                  d="M2.83719 3.09485H13.6693C14.3815 3.09485 14.9589 3.67219 14.9589 4.38439V12.1216C14.9589 12.8338 14.3815 13.4112 13.6693 13.4112H2.83719C2.125 13.4112 1.54765 12.8338 1.54765 12.1216V4.38439C1.54765 3.67219 2.125 3.09485 2.83719 3.09485Z"
-                  stroke="white"
-                  stroke-width="0.884256"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-                <path
-                  d="M12.8954 5.1582L8.25304 8.76891L3.6107 5.1582"
-                  stroke="white"
-                  stroke-width="0.884256"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
+
+          <div className=" max-w-16 flex flex-row justify-start gap-3">
+            <div>
+              <img
+                className="max-w-8"
+                src="./src/assets/LinkedIn_icon_circle.svg.png"
+                alt="LinkedIn"
+              />
             </div>
-            <div className="my-auto">stevenbseo@icloud.com</div>
-          </div>
-          <div className="flex flex-row justify-start gap-6 py-10">
-            <div>LinkedIn</div>
-            <div>Github</div>
-            <div>X</div>
+            <div className="rounded-full ">
+              <img
+                className="max-w-8 rounded-full"
+                src="./src/assets/github-mark-white.png"
+                alt="GitHub"
+              />
+            </div>{" "}
+            <div className="rounded-full ">
+              <img
+                className="max-w-8 rounded-full"
+                src="./src/assets/Mail_(iOS).svg.png"
+                alt="GitHub"
+              />
+            </div>
           </div>
         </div>
       </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,32 +1,46 @@
 export function Main() {
   return (
     <div>
-      <div className=" max-w-3xl mx-auto px-14 flex flex-col py-12 place-items-center gap-5">
+      <div className=" max-w-5xl mx-auto px-14 flex flex-col pt-12 place-items-center gap-5">
         <div className=" text-white ">
           <img src={"./src/assets/memoji.png"} />
         </div>
         <div className="grid grid-cols place-items-center pt-8">
-          <div className=" text-white  rows-span text-center font-mono text-5xl font-extrabold tracking-tight">
-            <p className="py-1">I do code and</p>
-            <p className="py-1">
-              make content
-              <span className=" bg-gradient-to-r from-orange-400 from-10% via-40% to-pink-600 bg-clip-text text-transparent">
-                {} about it!
+          <div className=" text-white rows-span text-center font-mono text-4xl font-extrabold tracking-normal ">
+            <p className="py-5">
+              Hi! I'm
+              <span className=" bg-gradient-to-r from-yellow-500 from-30% via-60% to-rose-400 bg-clip-text text-transparent">
+                {} Steven
               </span>
+              <span>.</span>
+              <p className="py-8 leading-normal">
+                A fullstack developer building bridges between
+                <span className=" bg-gradient-to-r from-lime-500 from-20% via-60% to-orange-400 bg-clip-text text-transparent">
+                  {} imagination
+                </span>
+                {} and
+                <span className=" bg-gradient-to-r from-blue-300 from-20% via-60% to-pink-700 bg-clip-text text-transparent">
+                  {} innvoation
+                </span>
+                {} through cutting-edge software and web development
+              </p>
             </p>
           </div>
         </div>
       </div>
 
-      <div className=" max-w-3xl mx-auto px-8 text-white font-thin text-center text-base font-semilight tracking-wide leading-6">
-        I am a seasoned full-stack software engineer with over 8 years of
-        professional experience, specializing in backend development. My
-        expertise lies in crafting robust and scalable SaaS-based architectures
-        on the Amazon AWS platform.
+      <div className=" max-w-3xl mx-auto text-gray-300 px-5 text-center text-lg font-light tracking-wider leading-7">
+        Following a successful career in the NGO and Pharmacy sectors, I am now
+        seeking a career in Software Development. My academic achievements
+        include a Bachelor’s and Master’s degree in Information Science and a
+        Bachelor’s degree in Pharmacy. I am highly skilled and committed to
+        becoming a successful Software Developer. I have gained valuable
+        experience and knowledge in industry best practices as a junior
+        full-stack developer and web developer.
       </div>
       <div className="text-white grid grid-cols-5 py-10">
         <div></div>
-        <div className="col-span-3 flex flex-row flex-wrap place-content-center justify-center gap-5 font-bold text-lg">
+        <div className="col-span-3 flex flex-row flex-wrap place-content-center justify-center gap-10 font-bold text-lg">
           <button className=" border-white border-2 px-8 py-3 rounded-full bg-white text-black">
             Get In Touch
           </button>
@@ -37,43 +51,88 @@ export function Main() {
       </div>
       <div className="flex flex-col items-center ">
         <div className="py-16 uppercase text-stone-300 text-2xl font-bold tracking-widest">
-          <span className=" bg-gradient-to-r from-pink-600 from-10% via-40% to-blue-400 bg-clip-text text-transparent">
+          <span className=" bg-gradient-to-r from-amber-200 from-10% via-40% to-blue-300 bg-clip-text text-transparent">
             Experience with:
           </span>
         </div>
+
         <div className="flex flex-row flex-wrap justify-center text-stone-300 gap-x-10 gap-y-3 px-16">
-          <div className="bg-white rounded-lg p-1">
-            <img className=" h-12" src="./src/assets/JS.svg" alt="" />
+          <div className="grayscale hover:grayscale-0 rounded-lg p-1">
+            <img
+              className=" h-14 bg-white rounded-lg"
+              src="./src/assets/JS.svg"
+              alt=""
+            />
           </div>
-          <div className="bg-white rounded-lg p-1">
-            <img className=" h-12" src="./src/assets/TS.png" alt="" />
+          <div className=" grayscale hover:grayscale-0 rounded-lg p-1">
+            <img
+              className=" h-14 bg-white rounded-lg"
+              src="./src/assets/TS.png"
+              alt=""
+            />
           </div>
-          <div className="bg-white rounded-lg p-1">
-            <img className=" h-12" src="./src/assets/node_js.jpg" alt="" />
+          <div className=" grayscale hover:grayscale-0 rounded-lg p-1">
+            <img
+              className=" h-14 bg-white rounded-lg"
+              src="./src/assets/node_js.jpg"
+              alt=""
+            />
           </div>
-          <div className="bg-white rounded-lg p-1">
-            <img className=" h-12" src="./src/assets/React.png" alt="" />
+          <div className=" grayscale hover:grayscale-0 rounded-lg p-1">
+            <img
+              className=" h-14 bg-white rounded-lg"
+              src="./src/assets/React.png"
+              alt=""
+            />
           </div>
-          <div className="bg-white rounded-lg p-1">
-            <img className=" h-12" src="./src/assets/HTML5.png" alt="" />
+          <div className=" grayscale hover:grayscale-0 rounded-lg p-1">
+            <img
+              className=" bg-white p-1 h-14 rounded-lg"
+              src="./src/assets/HTML5.png"
+              alt=""
+            />
           </div>
-          <div className="bg-white rounded-lg p-1">
-            <img className=" h-12" src="./src/assets/CSS.png" alt="" />
+          <div className="  grayscale hover:grayscale-0 p-1">
+            <img
+              className=" bg-white rounded-lg h-14"
+              src="./src/assets/CSS.png"
+              alt=""
+            />
           </div>
-          <div className="bg-white rounded-lg p-1">
-            <img className=" h-12" src="./src/assets/Tailwind_css.png" alt="" />
+          <div className=" grayscale hover:grayscale-0 rounded-lg p-1">
+            <img
+              className=" h-14 bg-white rounded-lg"
+              src="./src/assets/Tailwind_css.png"
+              alt=""
+            />
           </div>
-          <div className="bg-white rounded-lg p-1">
-            <img className=" h-12" src="./src/assets/github.png" alt="" />
+          <div className=" rounded-lg p-1">
+            <img
+              className=" h-14 bg-white p-1 rounded-lg"
+              src="./src/assets/github.png"
+              alt=""
+            />
           </div>
-          <div className="bg-white rounded-lg p-1">
-            <img className=" h-12" src="./src/assets/WordPress.png" alt="" />
+          <div className=" grayscale hover:grayscale-0 rounded-lg p-1">
+            <img
+              className=" h-14 bg-white p-1 rounded-lg"
+              src="./src/assets/WordPress.png"
+              alt=""
+            />
           </div>
-          <div className="bg-white rounded-lg p-1">
-            <img className=" h-12" src="./src/assets/psql.png" alt="" />
+          <div className=" grayscale hover:grayscale-0 rounded-lg p-1">
+            <img
+              className=" h-14 bg-white p-1 rounded-lg"
+              src="./src/assets/psql.png"
+              alt=""
+            />
           </div>
-          <div className="bg-white rounded-lg p-1">
-            <img className=" h-12" src="./src/assets/mongodb.png" alt="" />
+          <div className=" grayscale hover:grayscale-0 rounded-lg p-1">
+            <img
+              className=" h-14 bg-white p-1 rounded-lg"
+              src="./src/assets/mongodb.png"
+              alt=""
+            />
           </div>
         </div>
       </div>
