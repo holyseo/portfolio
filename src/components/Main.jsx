7c8bc8fa32b3666d74134f2bb9937f5aa4cd1e59
holyseo <128103221+holyseo@users.noreply.github.com>
2024-05-24 15:59:42 +0100

    update personal statement

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -15,20 +15,20 @@ export function Main() {
 
   return (
     <div>
-      <div className=" max-w-5xl mx-auto px-14 flex flex-col pt-12 place-items-center gap-5">
-        <div className=" text-white ">
+      <div className="flex flex-col max-w-5xl gap-5 pt-12 mx-auto  px-14 place-items-center">
+        <div className="text-white ">
           <img src={"assets/memoji.png"} alt="memoji" />
         </div>
-        <div className="grid grid-cols place-items-center pt-8">
-          <div className=" text-white rows-span text-center font-mono text-lg md:text-2xl xl:text-4xl font-extrabold tracking-normal">
-            <p className=" py-5">
+        <div className="grid pt-8 grid-cols place-items-center">
+          <div className="font-mono text-lg font-extrabold tracking-normal text-center text-white  rows-span md:text-2xl xl:text-4xl">
+            <p className="py-5 ">
               Hi! I'm
               <span className=" bg-gradient-to-r from-yellow-500 from-30% via-60% to-rose-400 bg-clip-text text-transparent">
                 {} Steven
               </span>
               <span>.</span>
             </p>
-            <p className=" leading-normal">
+            <p className="leading-normal ">
               A Frontend developer building bridges between
               <span className=" bg-gradient-to-r from-lime-500 from-20% via-60% to-blue-600 bg-clip-text text-transparent">
                 {} imagination
@@ -43,13 +43,12 @@ export function Main() {
         </div>
       </div>
 
-      <div className="flex flex-col gap-5 py-8 max-w-4xl mx-auto text-gray-300 px-5 text-sm font-mono leading-1 md:leading-5 xl:text-lg xl:leading-7 ">
+      <div className="flex flex-col max-w-4xl gap-5 px-5 py-8 mx-auto font-mono text-sm text-gray-300 leading-1 md:leading-5 xl:text-lg xl:leading-7 ">
         <div className="">
-          Following a successful career in the NGO and Pharmacy sectors, I am
-          now seeking a career in Software Development, with a specific focus on
-          becoming a Front End Software Developer. My academic achievements,
-          including a Master’s degree in Information Science, provide a solid
-          foundation for advancing software development.
+          I am excited to return to the Software Development industry after a
+          successful career in the NGO and Pharmacy sectors. My academic
+          achievements, including a Master’s degree in Information Science,
+          provide a solid foundation for advancing software development.
         </div>
 
         <div>
@@ -67,31 +66,31 @@ export function Main() {
           development.
         </div>
       </div>
-      <div className="text-white grid grid-cols-5 md:py-5 xl:py-10">
+      <div className="grid grid-cols-5 text-white md:py-5 xl:py-10">
         <div></div>
-        <div className="col-span-3 flex flex-row flex-wrap place-content-center justify-center gap-3 text-sm md:gap-7 md:text-md xl:gap-10 xl:text-lg font-bold">
+        <div className="flex flex-row flex-wrap justify-center col-span-3 gap-3 text-sm font-bold place-content-center md:gap-7 md:text-md xl:gap-10 xl:text-lg">
           <a href="mailto:stevenbseo@icloud.com">
-            <button className=" border-white border-2 px-8 py-3 rounded-full bg-white text-black">
+            <button className="px-8 py-3 text-black bg-white border-2 border-white rounded-full ">
               Get In Touch
             </button>
           </a>
           <a href="assets/Steven_Seo_CV_Portfolio" download>
-            <button className=" border-white border-2 px-8 py-3 rounded-full ">
+            <button className="px-8 py-3 border-2 border-white rounded-full ">
               Download CV
             </button>
           </a>
         </div>
       </div>
       <div className="flex flex-col items-center ">
-        <div className="py-16 uppercase text-stone-300 font-bold tracking-widest">
+        <div className="py-16 font-bold tracking-widest uppercase text-stone-300">
           <span className=" bg-gradient-to-r from-amber-500 from-10% via-40% to-sky-500 text-2xl bg-clip-text text-transparent">
             Experience with:
           </span>
         </div>
 
-        <div className="flex flex-row flex-wrap justify-center text-stone-300 gap-x-10 gap-y-3 px-16">
+        <div className="flex flex-row flex-wrap justify-center px-16 text-stone-300 gap-x-10 gap-y-3">
           {img_src.map((i, index) => (
-            <div key={index} className=" hover:animate-pulse rounded-lg">
+            <div key={index} className="rounded-lg  hover:animate-pulse">
               <img
                 className=" w-12 bg-white rounded-lg p-0.5"
                 src={`assets/${i}`}
